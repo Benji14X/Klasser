e3b7f70bca4afd2af5cfc01b938279908d11501b
benjamin.fatahi <benjamin.fatahi@elev.ga.ntig.se>
2024-04-26 21:44:55 +0200

    yeywldsv

--- src/Main.java
+++ src/Main.java
@@ -1,121 +1,146 @@
 public class Main {
     public static void main(String[] args) {
-        public class Book{
-            private String name; //variabel för att hålla bokens namn
-            private int pages; // variabel för att hålla antalet sidor i boken
-            private int currentPage; // variabel för att hålla nuvarande sida som läsaren är på
+        // Skapa instanser av bok, sten, arbetare och datordel
+        Book myBook = new Book("Java Basics", 120);
+        Rock myRock = new Rock(15.5);
+        Worker myWorker = new BlueCollar("John", 30);
+        ComputerPart myPart = new ComputerPart("GPU", 299.99, "Graphics Card");
 
-            //Konstruktor för att skapa en ny bok med namn och sidantal
-            public Book(String name, int pages){
-                this.name = name;
-                this.pages = pages;
-                this.currentPage = 0;//starta från första sidan
-            }
-            //Metod för att hämta nuvarande sidnummer
-            public int getCurrentPage(){
-                return currentPage;
-            }
+        // Anropa metoder för att visa information om skapade objekt
+        System.out.println("Book Name: " + myBook.getName());
+        System.out.println("Book Pages: " + myBook.getPages());
+        System.out.println("Rock Weight: " + myRock.getWeight());
+        System.out.println("Worker Name: " + myWorker.getName());
+        System.out.println("Worker Age: " + myWorker.getAge());
+        System.out.println("Computer Part Type: " + myPart.getType());
+        System.out.println("Computer Part Price: " + myPart.getPrice());
+    }
+}
 
-            //Metod för att hämta bokens namn
-            public String getName(){
-                return name;
-            }
-            //Metod för att hämta det totala antalet sidor i boken
-            public int getPages(){
-                return pages;
-            }
-            public class Rock {
-                private double weight; // privat variabel för att lagra stenens vikt
+// Klassdefinition för Book
+class Book {
+    private String name;
+    private int pages;
+    private int currentPage;
 
-                //konstruktor för att skapa en ny sten med en specefik vikt
-                public Rock(double weight) {
-                    this.weight = weight;
-                }
-                //Metod för att hämta vikten av stenen
-
-                public double getWeight() {
-                    return weight;
-                }
-            }
-            public class Worker {
-                protected String name; //namn på arbetaren
-                protected int age; //ålder på arbetaren
-
-                //Konstruktor som tar namn och ålder som argument
-                public Worker(String name, int age){
-                    this.name = name;
-                    this.age = age;
-                }
-                //metod för att hämta namnet på arbetaren
-
-                public String getName() {
-                    return name;
-                }
-                //Metod för att åldern på arbetaren
-
-                public int getAge() {
-                    return age;
-                }
-            }
-            public class BlueCollar extends worker {
-                public BlueCollar(String name, int age ){
-                    super(name, age);
-                }
-                //Metod som simulerar fysiskt arbete
-                public void work(){
-                    System.out.println(name + "is working hard");
-                }
-            }
-            public class WhiteCollar extends Worker {
-                public class WhiteCollar(String name, int age){
-                    super(name, age);
-                }
-                //Metod som simulerar kontorsarbete
-                public void work(){
-                    System.out.println(name + "is working in a office");
-                }
-            }
-            public class Hardware {
-                private String name; // namn på hårdvarudelen
-                private double price; // pris på hårdvarudelen
-
-                // Konstruktor som initialiserar namn och pris
-                public Hardware(String name, double price) {
-                    this.name = name;
-                    this.price = price;
-                }
-
-                // Metod för att hämta namnet på hårdvarudelen
-                public String getName() {
-                    return name;
-                }
-
-                // Metod för att hämta priset på hårdvarudelen
-                public double getPrice() {
-                    return price;
-                }
-            }
-
-            public class ComputerPart extends Hardware {
-                private String type; // typ av datordel, t.ex. processor, grafikkort
-
-                // Konstruktor som även sätter typ av del
-                public ComputerPart(String name, double price, String type) {
-                    super(name, price);
-                    this.type = type;
-                }
-
-                // Metod för att hämta typ av datordel
-                public String getType() {
-                    return type;
-                }
-            }
-
-
-
-
-        }
+    // Konstruktor för Book
+    public Book(String name, int pages) {
+        this.name = name;
+        this.pages = pages;
+        this.currentPage = 0;
     }
 
-    
+    // Metod för att få den nuvarande sidan i boken
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    // Metod för att få namnet på boken
+    public String getName() {
+        return name;
+    }
+
+    // Metod för att få antalet sidor i boken
+    public int getPages() {
+        return pages;
+    }
+}
+
+// Klassdefinition för Rock
+class Rock {
+    private double weight;
+
+    // Konstruktor för Rock
+    public Rock(double weight) {
+        this.weight = weight;
+    }
+
+    // Metod för att få vikten av stenen
+    public double getWeight() {
+        return weight;
+    }
+}
+
+// Bas klassdefinition för Worker
+class Worker {
+    protected String name;
+    protected int age;
+
+    // Konstruktor för Worker
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Metod för att få arbetarens namn
+    public String getName() {
+        return name;
+    }
+
+    // Metod för att få arbetarens ålder
+    public int getAge() {
+        return age;
+    }
+}
+
+// Subklass för BlueCollar som ärver från Worker
+class BlueCollar extends Worker {
+    public BlueCollar(String name, int age) {
+        super(name, age);
+    }
+
+    // Metod som simulerar fysiskt arbete
+    public void work() {
+        System.out.println(name + " is working hard");
+    }
+}
+
+// Subklass för WhiteCollar som ärver från Worker
+class WhiteCollar extends Worker {
+    public WhiteCollar(String name, int age) {
+        super(name, age);
+    }
+
+    // Metod som simulerar kontorsarbete
+    public void work() {
+        System.out.println(name + " is working in an office");
+    }
+}
+
+// Bas klassdefinition för Hardware
+class Hardware {
+    private String name;
+    private double price;
+
+    // Konstruktor för Hardware
+    public Hardware(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Metod för att få namnet på hårdvarudelen
+    public String getName() {
+        return name;
+    }
+
+    // Metod för att få priset på hårdvarudelen
+    public double getPrice() {
+        return price;
+    }
+}
+
+// Subklass för ComputerPart som ärver från Hardware
+class ComputerPart extends Hardware {
+    private String type;
+
+    // Konstruktor för ComputerPart
+    public ComputerPart(String name, double price, String type) {
+        super(name, price);
+        this.type = type;
+    }
+
+    // Metod för att få typen av datordel
+    public String getType() {
+        return type;
+    }
 }

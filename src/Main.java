4f2b4f4c3e20abe76ae5381c192a6b4cc8357e9a
benjamin.fatahi <benjamin.fatahi@elev.ga.ntig.se>
2024-04-26 20:11:46 +0200

    yey

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public class Book{
+            private String name; //variabel för att hålla bokens namn
+            private int pages; // variabel för att hålla antalet sidor i boken
+            private int currentPage; // variabel för att hålla nuvarande sida som läsaren är på
+
+            //Konstruktor för att skapa en ny bok med namn och sidantal
+            public Book(String name, int pages){
+                
+            }
+        }
     }
 
     
